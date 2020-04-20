37ad66203cb02010fe09140a57884542fe05b9fb
mmm <510161501@qq.com>
2020-04-20 16:50:21 +0800

    init

---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>{{aaa}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      aaa: '123'
+    }
+  },
+  created(){
+    this.getData();
+  },
+  methods:{
+    getData(){
+      console.log(123)
+    }
+  }
+}
+</script>
